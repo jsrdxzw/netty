0f7748992569d9c11d11c5fedec58235a83eab34
xuzhiwei <xuzhiwei@kezaihui.com>
2020-04-21 19:56:35 +0800

    netty init

---
+++ src/main/java/com/jsrdxzw/netty/NettyClient.java
@@ -0,0 +1,35 @@
+package com.jsrdxzw.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.util.Date;
+
+/**
+ * @author xuzhiwei
+ * @date 2020-04-21
+ */
+public class NettyClient {
+    public static void main(String[] args) throws InterruptedException {
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        bootstrap
+                .group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<>() {
+                    @Override
+                    protected void initChannel(Channel channel) throws Exception {
+                        channel.pipeline().addLast(new StringEncoder());
+                    }
+                });
+        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
+        while (true) {
+            channel.writeAndFlush(new Date() + ": hello world!");
+            Thread.sleep(2000);
+        }
+    }
+}
